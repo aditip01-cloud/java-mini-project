779f3a52966eb04079a90ece5a861e0601b6df2e
ketakioak-123 <ketaki.oak@cumminscollege.in>
2026-05-02 09:56:12 +0530

    Create Client.java

---
+++ Client.java
@@ -0,0 +1,15 @@
+public class Client {
+    private String name;
+    private LocalDate dateOfBirth;
+
+    public Client(String name, LocalDate dateOfBirth) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getAge() {
+        return Period.between(dateOfBirth, LocalDate.now()).getYears();
+    }
+
+   
+}
